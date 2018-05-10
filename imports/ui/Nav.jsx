e3207ad41ad9e3cdaebfbd46e905988f95ad441c
Carlos Penaloza <c.penaloza@uniandes.edu.co>
2018-05-10 18:17:32 -0500

    Style for google login

--- imports/ui/Nav.jsx
+++ imports/ui/Nav.jsx
@@ -5,30 +5,25 @@ class Nav extends Component {
 
     render() {
         return (
+            <div>
                 <div>
-                    <div>
-                        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                            <div className="container">
-                                <a className="navbar-brand" href="/">NextBus Visualization</a>
-                                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                        aria-expanded="false" aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"/>
-                                </button>
-
-                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul className="navbar-nav ml-auto">
-
-                                        <li className="nav-item center-login"><AccountsUIWrapper/></li>
-
-                                    </ul>
-                                </div>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                        <div className="container">
+                            <a className="navbar-brand" href="/">NextBus Visualization</a>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"/>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav ml-auto">
+                                    <li className="nav-item center-login"><AccountsUIWrapper/></li>
+                                </ul>
                             </div>
-                        </nav>
-                    </div>
-
+                        </div>
+                    </nav>
                 </div>
-
+            </div>
         );
     }
 }
